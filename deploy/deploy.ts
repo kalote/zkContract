3b16f46391f68ec33d9674894a11ad5850a0eeb4
kalote <johannbich@gmail.com>
2022-11-07 13:22:00 +0100

    feat: remove greeter contract / update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -13,7 +13,7 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
     // Initialize the wallet.
     const providerL2 = new Provider(hre.userConfig.zkSyncDeploy?.zkSyncNetwork);
-    const wallet = new Wallet(<string>process.env.PRIVATEKEY, providerL2);
+    const wallet = new Wallet(<string>process.env.PRIVATE_KEY, providerL2);
 
     // Create deployer object and load the artifact of the contract you want to deploy.
     const deployer = new Deployer(hre, wallet);
@@ -21,21 +21,20 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
     // Estimate contract deployment fee
     const deploymentFee = await deployer.estimateDeployFee(artifact, []);
-
-    // Deposit funds to L2
-    const depositHandle = await deployer.zkWallet.deposit({
-      to: deployer.zkWallet.address,
-      token: utils.ETH_ADDRESS,
-      amount: deploymentFee.mul(2),
-    });
-    // Wait until the deposit is processed on zkSync
-    await depositHandle.wait();
-
-    // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
     // `greeting` is an argument for contract constructor.
     const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
     console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
+    // Deposit funds to L2
+    // const depositHandle = await deployer.zkWallet.deposit({
+    //   to: deployer.zkWallet.address,
+    //   token: utils.ETH_ADDRESS,
+    //   amount: deploymentFee.mul(2),
+    // });
+    // Wait until the deposit is processed on zkSync
+    // await depositHandle.wait();
+
+    // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
     const ZkTwittContract = await deployer.deploy(artifact, []);
 
     // Show the contract info.
